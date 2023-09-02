595d13a9dd294b0e267d4b4f8f20b702f4b220d8
Meysam Yavarikhoo <135262495+Meysamy71@users.noreply.github.com>
2023-09-02 20:23:41 +0330

    Update docker-backup.sh V1.0.1

--- docker-backup.sh
+++ docker-backup.sh
@@ -5,6 +5,16 @@ LOG_PATH=/var/dockers/log
 LOG_DATE=`date +%Y.%m.%d.%H:%M`
 CONTAINER_NUMBER=`docker ps --format '{{.Names}}'|wc -l`
 
+func_log(){
+
+if [[ $1 -eq 0 ]]
+    then
+        echo "Docker $5 $2 in $3 Successfully" >> $4/$2/$2.log
+    else
+        echo "Docker $5 $2 in $3 Error" >> $4/$2/$2.log
+    fi
+}
+
 for ((i=1;i<=$CONTAINER_NUMBER;i++))
 do
     CONTAINER_NAME=`docker ps --format '{{.Names}}'|head -n $i |tail -n 1`
@@ -20,36 +30,23 @@ do
     fi
 #Docker Container Commit	    
     docker commit $CONTAINER_NAME $CONTAINER_NAME:backup >> /dev/null 2>&1
-    if [[ $? -eq 0 ]]
-    then
-	echo "Docker commit $CONTAINER_NAME in $LOG_DATE Successfully" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    else	
-	echo "Docker commit $CONTAINER_NAME in $LOG_DATE Error" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    fi
+    
+    func_log $? $CONTAINER_NAME $LOG_DATE $LOG_PATH "Commit"
+
 #Docker Image Save and Zip    
     docker image save $CONTAINER_NAME:backup | xz  > $BACKUP_PATH/$CONTAINER_NAME/$CONTAINER_NAME.$LOG_DATE.tar.xz 
-    if [[ $? -eq 0 ]]
-    then
-	echo "Docker image save $CONTAINER_NAME in $LOG_DATE Successfully" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    else	
-	echo "Docker image save $CONTAINER_NAME in $LOG_DATE Error" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    fi
+    
+    func_log $? $CONTAINER_NAME $LOG_DATE $LOG_PATH "Image save"
+
 #Rsync Backup
     rsync -az $BACKUP_PATH/$CONTAINER_NAME/ devops@192.168.18.132:/home/devops/dockers/$CONTAINER_NAME >> /dev/null 2>&1
-    if [[ $? -eq 0 ]]
-    then
-	echo "Docker image scp $CONTAINER_NAME in $LOG_DATE Successfully" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    else	
-	echo "Docker image scp $CONTAINER_NAME in $LOG_DATE Error" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    fi
+    
+    func_log $? $CONTAINER_NAME $LOG_DATE $LOG_PATH "Sync"
+
 #Docker Image Remove Container Backup Tag    
     docker image rm $CONTAINER_NAME:backup >> /dev/null 2>&1
-    if [[ $? -eq 0 ]]
-    then
-	echo "Docker image remove $CONTAINER_NAME:backup in $LOG_DATE Successfully" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    else	
-	echo "Docker image remove $CONTAINER_NAME:backup in $LOG_DATE Error" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
-    fi
+    
+    func_log $? $CONTAINER_NAME $LOG_DATE $LOG_PATH "Image remove"
 
 echo "=================" >> $LOG_PATH/$CONTAINER_NAME/$CONTAINER_NAME.log
 
@@ -57,4 +54,3 @@ done
 
 #Backup rm 2 days
 find $BACKUP_PATH -name *.tar.xz -mtime 2 -exec rm -rf {} \;
-
